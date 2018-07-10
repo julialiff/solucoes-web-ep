c128b93af4c56dc950cc2507c82d2e55dc6e5076
Julia Litvinoff Justus <julialiff@hotmail.com>
2018-07-09 22:54:21 -0300

    POI

--- teste_julia.php
+++ teste_julia.php
@@ -69,6 +69,12 @@ ini_set('display_errors', 'On');
           zoom: 13,
           mapTypeId: 'roadmap',
           styles:[{
+            "featureType": "poi",
+            "stylers": [{
+              "visibility": "off"
+              }]
+            },
+            {
             "featureType": "transit.station.bus",
             "stylers": [{
               "visibility": "off"
